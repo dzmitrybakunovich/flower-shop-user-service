44517c96aaa7c57448933df6d2e532c584c44b3a
Dzmitry Bakunovich <dv.bakunovich@gmail.com>
2022-04-17 23:10:10 +0300

    Rewritten settings and manage.py

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -1,15 +1,17 @@
+import os
+import sys
 from pathlib import Path
 
-# Build paths inside the pyproject like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+PROJECT_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(os.path.join(PROJECT_DIR, 'apps'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^e%jt1vcqs#ko07-e$q=k96ix42&wiab%38-hmiowkc%#k%$&n'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
@@ -19,6 +21,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'drf_spectacular',
+
+    'users',
+    'notifications',
 ]
 
 MIDDLEWARE = [
@@ -31,7 +40,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'src.urls'
 
 TEMPLATES = [
     {
@@ -49,7 +58,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'src.wsgi.application'
 
 # DATABASE
 DATABASES = {
@@ -66,6 +75,7 @@ DATABASES = {
 # db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 # DATABASES['default'].update(db_from_env)
 
+AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -83,17 +93,43 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SIMPLE_JWT = {
+    'USER_ID_FIELD': 'uuid',
+    'USER_ID_CLAIM': 'user_uuid',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'FSU service',
+    'DESCRIPTION': 'Flower shop users service',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
+
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://redis:6379')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://redis:6379')
+CELERY_TIMEZONE = 'Europe/Minsk'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = ''
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 api_v1_urlpatterns: list = [
     path('users/', include(('users.urls', 'users'), namespace='users')),
+    path(
+        'notifications/',
+        include(
+            ('notifications.urls', 'notifications'),
+            namespace='notifications')
+    ),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
 ]
 
 urlpatterns: list = [
